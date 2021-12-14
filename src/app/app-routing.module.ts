13be6d6e146cfe12a4660fc724f670716490bd60
MarzieHallajian <marzieh.hallajian@gmail.com>
2021-12-14 17:00:08 +0330

    add navbar and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphComponent } from './Components/graph/graph.component';
+import { HomeComponent } from './Components/home/home.component';
+import { UploadFileComponent } from './Components/upload-file/upload-file.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'graph', component: GraphComponent },
+  { path: 'upload-file', component: UploadFileComponent }  ,
+  { path:'home',component:HomeComponent },
+  { path:'', redirectTo:"/home", pathMatch:'full' },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GraphComponent } from './Components/graph/graph.component';
+import { HomeComponent } from './Components/home/home.component';
+import { UploadFileComponent } from './Components/upload-file/upload-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent,
+    HomeComponent,
+    UploadFileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
